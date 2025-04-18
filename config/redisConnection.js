5db7fe033c6f9ab52b084a13f9b66847b85108e6
Ameer64807 <ameer@bearplex.com>
2025-04-18 20:12:11 +0500

    changs

--- config/redisConnection.js
+++ config/redisConnection.js
@@ -5,7 +5,7 @@ console.log("🔍 REDIS_URL:", process.env.REDIS_URL);
 
 const connection = new Redis("redis://cronmicroservice-imqdd1.serverless.usw2.cache.amazonaws.com:6379", {
   maxRetriesPerRequest: null, // 🔥 Required by BullMQ
-  // tls: {}, // ✅ Enable TLS for ElastiCache Serverless
+  tls: {}, // ✅ Enable TLS for ElastiCache Serverless
 });
 
 module.exports = connection;
