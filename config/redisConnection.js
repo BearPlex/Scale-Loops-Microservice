59fd53a7978e657b1068f7d6e5b21a571b763ec2
Ameer64807 <ameer@bearplex.com>
2025-04-18 17:55:16 +0500

    cron microservice

--- config/redisConnection.js
+++ config/redisConnection.js
@@ -2,7 +2,7 @@
 const Redis = require('ioredis');
 
 console.log("process.env.REDIS_URLprocess.env.REDIS_URL", process.env.REDIS_URL);
-const connection = new Redis(process.env.REDIS_URL, {
+const connection = new Redis("rediss://cronmicroservice-imqdd1.serverless.usw2.cache.amazonaws.com:6379", {
   maxRetriesPerRequest: null, // 🔥 Required by BullMQ
 });
 
